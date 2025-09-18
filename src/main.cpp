e632c142cbff82680057b611c7ef570f988d1f60
Tom <tjreit@live.nl>
2025-09-18 11:08:11 +0200

    kleine update

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,22 @@
 #include <Wifihandler/Wifihandler.h>
 #include "WebServer/WebServer.h"
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  TaskMonitor::begin(500);   // 500 ms venster + autocalibratie
-  TaskMonitor::printOnce();  // eerste snapshot
+  TaskMonitor::begin(500);  // 500 ms venster + autocalibratie
+  TaskMonitor::printOnce(); // eerste snapshot
   WiFiService.begin("mijn-esp32",
                     WiFiModeSel::AP,
-                    "MijnSSID", "MijnPass",/*Wifi netwerk*/
-                    "SpuitMeDeBekVol", "SterkAPWachtwoord"/*Accespoint netwerk*/,
+                    "MijnSSID", "MijnPass", /*Wifi netwerk*/
+                    "SpuitMeDeBekVol", "SterkAPWachtwoord" /*Accespoint netwerk*/,
                     /*useWiFiManager=*/false);
   OTAService.begin("mijn-esp32", "OTApassword", 3232);
   WebServerService.begin();
+}//hihi
 
-}
-
-void loop() {
+void loop()
+{
   WiFiService.loop();
   OTAService.loop();
   TaskMonitor::loop(10000); // elke 10s een statusregel
