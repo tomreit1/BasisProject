40923f2c756f64503066961082b69d6a827eeab7
Tom <tjreit@live.nl>
2025-09-18 19:46:01 +0200

    asdf

--- src/Secrets.h
+++ src/Secrets.h
@@ -1,6 +1,6 @@
 #pragma once
-#define WIFI_SSID ""
-#define WIFI_PASS ""
+#define WIFI_SSID "d"
+#define WIFI_PASS "c"
 
-#define WIFI_AP_SSID ""
-#define WIFI_AP_PASS ""
+#define WIFI_AP_SSID "b"
+#define WIFI_AP_PASS "a"
